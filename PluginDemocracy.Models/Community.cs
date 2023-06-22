48fa59c6b9f83976b17f5576dae476d6e9bb882d
joe-gregory <josephedward2nd@gmail.com>
2023-06-21 21:28:55 -0700

    Add project files.

---
+++ PluginDemocracy.Models/Community.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemocracy.Models
+{
+    internal class Community
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+
+    }
+}
