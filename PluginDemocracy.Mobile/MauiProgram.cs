b1695fa3036b4d0b1a6928ca017b81a824101c11
joe-gregory <josephedward2nd@gmail.com>
2023-11-14 15:13:43 -0800

    API and Mobile projects created

--- PluginDemocracy.Mobile/MauiProgram.cs
+++ PluginDemocracy.Mobile/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace PluginDemocracy.Mobile
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+    		builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- PluginDemocracy.Mobile/Platforms/Windows/App.xaml.cs
+++ PluginDemocracy.Mobile/Platforms/Windows/App.xaml.cs
@@ -21,4 +21,5 @@ namespace PluginDemocracy.Mobile.WinUI
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
+
 }
