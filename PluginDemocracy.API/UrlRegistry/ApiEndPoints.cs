c586000f1eb7e6f23a1e5ad4b8e9e1647405613b
joe-gregory <josephedward2nd@gmail.com>
2024-02-29 12:36:05 -0800

    first completed version of UIComponents.RegisterCommunity.razor

--- PluginDemocracy.API/UrlRegistry/ApiEndPoints.cs
+++ PluginDemocracy.API/UrlRegistry/ApiEndPoints.cs
@@ -3,10 +3,11 @@
     public static class ApiEndPoints
     {
         public static readonly string Base = "/api/";
-
+        #region Test
         public static readonly string TestController = Base + "test/";
         public static readonly string GetTestMessagesPage = TestController + "testmessagespage";
-
+        #endregion
+        #region User
         public static readonly string UsersController = Base + "users/";
         public static readonly string PostSignUp = UsersController + "signup";
         public static readonly string ConfirmEmail = UsersController + "confirmemail";
@@ -16,5 +17,10 @@
         public static readonly string PostToggleUserCulture = UsersController + "toggleuserculture";
         public static readonly string PostUpdateAccount = UsersController + "updateaccount";
         public static readonly string UpdateProfilePicture = UsersController + "updateprofilepicture";
+        #endregion
+        #region Community
+        public static readonly string CommunityController = Base + "community/";
+        public static readonly string RegisterCommunity = CommunityController + "registercommunity";
+        #endregion
     }
 }
