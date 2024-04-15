94db8716be0b7d596c5d77ae980bb303586f71e0
joe-gregory <josephedward2nd@gmail.com>
2024-04-15 12:44:51 -0700

    Post comment class to reflect comments on Posts

---
+++ PluginDemocracy.Models/Feed/PostComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemocracy.Models
+{
+    public class PostComment
+    {
+        public int Id { get; set; }
+        public User? Author { get; private set; }
+        public string? Comment { get; private set; }
+        public DateTime PublishedDate { get; private set; }
+        protected PostComment()
+        {
+            PublishedDate = DateTime.UtcNow;
+        }
+        public PostComment(User user, string body)
+        {
+            Author = user;
+            Comment = body;
+            PublishedDate = DateTime.UtcNow;
+        }
+    }
+}
