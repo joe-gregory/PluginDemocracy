fda5cf5a71469023778c6a1bee7780bfd2bdcf77
joe-gregory <josephedward2nd@gmail.com>
2024-04-15 12:43:43 -0700

    The most significant changes to the code involve the modification of the `Post` class. The `PublishedDate` property is now non-nullable, meaning every `Post` must have a `PublishedDate`. Additionally, new properties have been added to the `Post` class, including a list of `PostComment` objects called `Comments`, a nullable `DateTime` object called `LatestComment`, and a list of strings called `Images`. These properties will hold the comments, the date and time of the latest comment, and the images for a post, respectively. The constructors for the `Post` class have been updated to initialize these new properties. A new method, `AddComment`, has been added to add a comment to the `Comments` list and update the `LatestComment` property.
    List of changes:
    
    1. The `DateTime? PublishedDate` property has been changed to a non-nullable `DateTime PublishedDate` property, requiring a `Post` to always have a `PublishedDate` (Code Change 1).
    2. A new property `List<PostComment> Comments` has been added to hold all the comments for a post (Code Change 2).
    3. A new property `DateTime? LatestComment` has been added to hold the date and time of the latest comment on a post (Code Change 3).
    4. A new property `List<string> Images` has been added to hold all the images for a post (Code Change 4).
    5. The constructor `Post()` has been modified to initialize `PublishedDate` with the current date and time, and `Comments` and `Images` with empty lists (Code Change 5).
    6. The constructor `Post(User user, string body)` has been modified to initialize `Comments` and `Images` with empty lists (Code Change 6).
    7. A new method `AddComment(PostComment comment)` has been added. This method adds a comment to the `Comments` list and updates `LatestComment` with the current date and time (Code Change 7).

--- PluginDemocracy.Models/Feed/Post.cs
+++ PluginDemocracy.Models/Feed/Post.cs
@@ -5,13 +5,28 @@
         public int Id { get; set; }
         public User? Author { get; private set; }
         public string? Body { get; private set; }
-        public DateTime? PublishedDate { get; private set; }
-        protected Post() { }
+        public DateTime PublishedDate { get; private set; }
+        public readonly List<PostComment> Comments;
+        public DateTime? LatestComment { get; private set; } = null;
+        public List<string> Images { get; set; }
+        protected Post() 
+        {
+            PublishedDate = DateTime.UtcNow;
+            Comments = [];
+            Images = [];
+        }
         public Post(User user, string body)
         {
             Author = user;
             Body = body;
             PublishedDate = DateTime.UtcNow;
+            Comments = [];
+            Images = [];
+        }
+        public void AddComment(PostComment comment)
+        {
+            Comments.Add(comment);
+            LatestComment = DateTime.UtcNow;
         }
     }
 }
