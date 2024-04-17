8b31588ac3e5bf72a93169f6d602b87e594a7d54
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:43:54 -0700

    Update to theme to enforce white backgrounds on dark mode in browsers

--- PluginDemocracy.UIComponents/PluginDemocracyTheme.cs
+++ PluginDemocracy.UIComponents/PluginDemocracyTheme.cs
@@ -9,18 +9,20 @@ namespace PluginDemocracy.UIComponents
 {
     public static class PluginDemocracyTheme
     {
-        public static string PrimaryColor { get => "008644"; }
-        public static MudTheme Theme => new MudTheme()
+        public const string PrimaryColor = "008644";
+        public static MudTheme Theme => new()
         {
             Palette = new PaletteLight()
             {
                 Primary = PrimaryColor,
-                AppbarBackground = PrimaryColor
+                AppbarBackground = PrimaryColor, 
+                Background = "#ffffff", //white background
             }, 
             PaletteDark = new PaletteDark()
             {
                 Primary = PrimaryColor,
-                AppbarBackground = PrimaryColor
+                AppbarBackground = PrimaryColor,
+                Background = "#ffffff", //white background currently for dark mode as well since the rest of the colors are not implemented for dark yet.
             }
         };
     }
