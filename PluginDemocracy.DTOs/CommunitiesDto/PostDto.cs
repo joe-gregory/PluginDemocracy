454954e5248840065d8880ef75f98442783a3c54
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:41:38 -0700

    DTO of Models.Post

---
+++ PluginDemocracy.DTOs/CommunitiesDto/PostDto.cs
@@ -0,0 +1,28 @@
+﻿using PluginDemocracy.Models;
+
+namespace PluginDemocracy.DTOs
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public UserDto? Author { get; set; }
+        public string? Body { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public List<PostCommentDto> Comments { get; set; } = [];
+        public DateTime? LatestActivity { get; set; }
+        public List<string> Images { get; set; } = [];
+        public PostDto()
+        {
+            Id = 0;
+        }
+        public PostDto(Post post)
+        {
+            Id = post.Id;
+            if (post.Author != null) Author = new(post.Author);
+            Body = post.Body;
+            PublishedDate = post.PublishedDate;
+            LatestActivity = post.LatestActivity;
+        }
+
+    }
+}
