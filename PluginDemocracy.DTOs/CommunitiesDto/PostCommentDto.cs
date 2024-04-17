a4e84b6056be59e12bb0c38524dcb7665e350601
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:41:18 -0700

    PostCommentDto for comments on posts

---
+++ PluginDemocracy.DTOs/CommunitiesDto/PostCommentDto.cs
@@ -0,0 +1,12 @@
+﻿using PluginDemocracy.Models;
+
+namespace PluginDemocracy.DTOs
+{
+    public class PostCommentDto(PostComment comment)
+    {
+        public int Id { get; set; } = comment.Id;
+        public UserDto Author { get; set; } = new(comment.Author);
+        public string Comment { get; set; } = comment.Comment;
+        public DateTime PublishedDate { get; set; } = comment.PublishedDate;
+    }
+}
