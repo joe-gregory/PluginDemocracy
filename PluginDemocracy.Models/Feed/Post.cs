6349d05dc347eec881a7d455dac5e14ea4ff8fa5
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:42:30 -0700

    Post class for posts

--- PluginDemocracy.Models/Feed/Post.cs
+++ PluginDemocracy.Models/Feed/Post.cs
@@ -7,13 +7,15 @@
         public string? Body { get; private set; }
         public DateTime PublishedDate { get; private set; }
         public readonly List<PostComment> Comments;
-        public DateTime? LatestComment { get; private set; } = null;
+        public DateTime? LatestActivity { get; private set; } = null;
         public List<string> Images { get; set; }
+        public List<Reaction> Reactions { get; set; }
         protected Post() 
         {
             PublishedDate = DateTime.UtcNow;
             Comments = [];
             Images = [];
+            Reactions = [];
         }
         public Post(User user, string body)
         {
@@ -22,11 +24,12 @@
             PublishedDate = DateTime.UtcNow;
             Comments = [];
             Images = [];
+            Reactions = [];
         }
         public void AddComment(PostComment comment)
         {
             Comments.Add(comment);
-            LatestComment = DateTime.UtcNow;
+            LatestActivity = DateTime.UtcNow;
         }
     }
 }
