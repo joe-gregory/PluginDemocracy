d0692bc566f0ea0d80da272be563ada93f68472f
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:41:58 -0700

    DTO class of Models.Reaction

---
+++ PluginDemocracy.DTOs/CommunitiesDto/ReactionDto.cs
@@ -0,0 +1,27 @@
+﻿using PluginDemocracy.Models;
+
+namespace PluginDemocracy.DTOs.CommunitiesDto
+{
+    public class ReactionDto
+    {
+        public int Id { get; set; }
+        public UserDto Author { get; private set; }
+        public ReactionType ReactionType { get; set; }
+        protected ReactionDto()
+        {
+            Author = new();
+            ReactionType = ReactionType.Like;
+        }
+        public ReactionDto(UserDto user, ReactionType reaction)
+        {
+            Author = user;
+            ReactionType = reaction;
+        }
+        public ReactionDto(Reaction reaction)
+        {
+            Id = reaction.Id;
+            Author = new UserDto(reaction.Author);
+            ReactionType = reaction.ReactionType;
+        }
+    }
+}
