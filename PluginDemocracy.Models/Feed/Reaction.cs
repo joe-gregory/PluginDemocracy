7c5741fbdd807bc7c67c02357fb7c9e101c70f09
joe-gregory <josephedward2nd@gmail.com>
2024-04-17 14:42:51 -0700

    Reaction class for posts

---
+++ PluginDemocracy.Models/Feed/Reaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemocracy.Models
+{
+    public class Reaction
+    {
+        public int Id { get; set; }
+        public User Author { get; private set; }
+        public ReactionType ReactionType { get; set; }
+        protected Reaction()
+        {
+            Author = new();
+            ReactionType = ReactionType.Like;
+        }
+        public Reaction(User user, ReactionType reaction)
+        {
+            Author = user;
+            ReactionType = reaction;
+        }
+    }
+    public enum ReactionType
+    {
+        Like,
+        Dislike,
+    }
+}
