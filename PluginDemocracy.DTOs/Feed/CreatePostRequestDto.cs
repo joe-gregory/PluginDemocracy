4e460960942d0e0d66aa90e8e30b7d6d32b02283
joe-gregory <josephedward2nd@gmail.com>
2024-04-22 14:58:41 -0700

    New DTO type for creating posts

---
+++ PluginDemocracy.DTOs/Feed/CreatePostRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PluginDemocracy.DTOs
+{
+    public class CreatePostRequestDto
+    {
+        public int CommunityId { get; set; }
+        public string? Body { get; set; }
+        public List<IFormFile> Files { get; set; } = [];
+    }
+}
