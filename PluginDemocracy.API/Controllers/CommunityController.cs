d77b2d6bff0fe8ebacdf02ecaa675b9c77b294bb
joe-gregory <josephedward2nd@gmail.com>
2024-04-24 19:47:15 -0700

    Community controller for deleting posts

--- PluginDemocracy.API/Controllers/CommunityController.cs
+++ PluginDemocracy.API/Controllers/CommunityController.cs
@@ -316,6 +316,38 @@ namespace PluginDemocracy.API.Controllers
         public async Task<ActionResult<PDAPIResponse>> GetFeed([FromQuery] int communityId)
         {
             PDAPIResponse response = new();
+            try
+            {
+                
+                //Extract User from claims
+                User? existingUser = await _utilityClass.ReturnUserFromClaims(User, response);
+                if (existingUser == null)
+                {
+                    response.AddAlert("error", "User from claims not found");
+                    return BadRequest(response);
+                }
+                Community? community = await _context.Communities.Include(c => c.Posts).ThenInclude(p => p.Author).FirstOrDefaultAsync(c => c.Id == communityId);
+                if (community == null)
+                {
+                    response.AddAlert("error", "Community not found");
+                    return BadRequest(response);
+                }
+
+                foreach (Post post in community.PostsByLatestActivity) response.Posts.Add(new PostDto(post));
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.AddAlert("error", ex.Message);
+                return BadRequest(response);
+            }
+            
+        }
+        [Authorize]
+        [HttpDelete(ApiEndPoints.DeletePost)]
+        public async Task<ActionResult<PDAPIResponse>> DeletePost([FromQuery] int postId)
+        {
+            PDAPIResponse response = new();
             //Extract User from claims
             User? existingUser = await _utilityClass.ReturnUserFromClaims(User, response);
             if (existingUser == null)
@@ -323,15 +355,30 @@ namespace PluginDemocracy.API.Controllers
                 response.AddAlert("error", "User from claims not found");
                 return BadRequest(response);
             }
-            Community? community = await _context.Communities.Include(c => c.Posts).FirstOrDefaultAsync(c => c.Id == communityId);
-            if (community == null)
+            Post? post = await _context.Posts.Include(p => p.Author).FirstOrDefaultAsync(p => p.Id == postId);
+            if (post == null)
             {
-                response.AddAlert("error", "Community not found");
+                response.AddAlert("error", "Post not found");
                 return BadRequest(response);
             }
-            
-            foreach (Post post in community.Posts) response.Posts.Add(new PostDto(post));
-            return Ok(response);
+            if (post?.Author?.Id != existingUser.Id)
+            {
+                response.AddAlert("error", "User does not have permission to delete this post");
+                return BadRequest(response);
+            }
+            try
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+                response.AddAlert("success", "Post deleted successfully");
+                response.SuccessfulOperation = true;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.AddAlert("error", ex.Message);
+                return BadRequest(response);
+            }
         }
     }
 }
