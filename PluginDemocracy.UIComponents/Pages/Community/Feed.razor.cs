04c42e83eef7957ba78f2bd144d1f25e2c9a58ff
joe-gregory <josephedward2nd@gmail.com>
2024-05-08 18:26:12 -0700

    trying to make feed update after creating post

--- PluginDemocracy.UIComponents/Pages/Community/Feed.razor.cs
+++ PluginDemocracy.UIComponents/Pages/Community/Feed.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PluginDemocracy.API.UrlRegistry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,38 @@ namespace PluginDemocracy.UIComponents.Pages.Community
 {
     public partial class Feed: IDisposable
     {
+        public event Action? OnCommunitySelectChange;
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            //if AppState.PDAPIResponse.LastRefreshed is older than 30 seconds, refresh user data
+            if (AppState.PDAPIResponse.LastRefreshed < DateTime.UtcNow.AddSeconds(-30)) await Services.GetDataAsync(ApiEndPoints.RefreshUserData);
+            //If user doesn't have any communities, redirect to JoinOrRegisterCommunity page
+            if (AppState.User?.Citizenships.Count == 0) Services.NavigateTo(FrontEndPages.JoinOrRegisterCommunity);
+
+            //If user has only one community, set it as selected. By setting it automatically, GetFeed should be called in the property setter
+            if (AppState.User?.Citizenships.Count == 1) AppState.SelectedCommunityInFeed = AppState.User.Citizenships[0].Id;
+            //If user has multiple communities, let the user select a community
+            //If a community had been selected previously, default to that one
+            if (AppState.SelectedCommunityInFeed != null) await OnCommunityChanged(AppState.SelectedCommunityInFeed);
+            AppState.OnChange += StateHasChangedSafe;
+        }
+
+        private async Task OnCommunityChanged(int? newCommunityId)
+        {
+            if (newCommunityId != null) AppState.SelectedCommunityInFeed = newCommunityId;
+            await GetFeed();
+        }
+        private async Task GetFeed()
+        {
+            string url = ApiEndPoints.GetFeed + $"?communityId={AppState.SelectedCommunityInFeed}";
+            await Services.GetDataAsync(url);
+            StateHasChanged();
+        }
+        private void StateHasChangedSafe()
+        {
+            InvokeAsync(StateHasChanged);
+        }
         public void Dispose()
         {
             AppState.OnChange -= StateHasChanged;
