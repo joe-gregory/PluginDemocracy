b52e46a62d76bc3adefc37c996ba50c1529e5eeb
joe-gregory <josephedward2nd@gmail.com>
2024-05-15 15:11:24 -0700

    New property to more easily identify parent post

--- PluginDemocracy.DTOs/Feed/PostCommentDto.cs
+++ PluginDemocracy.DTOs/Feed/PostCommentDto.cs
@@ -4,20 +4,27 @@ namespace PluginDemocracy.DTOs
 {
     public class PostCommentDto
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public int PostId { get; set; }
         public UserDto Author { get; set; } 
         public string Comment { get; set; } 
         public DateTime PublishedDate { get; set; } 
         public PostCommentDto()
         {
-            Id = 0;
-            Author = new();
             Comment = string.Empty;
+            Author = new UserDto();
+        }
+        public PostCommentDto(int postId, UserDto author, string comment)
+        {
+            PostId = postId;
+            Author = author;
+            Comment = comment;
             PublishedDate = DateTime.UtcNow;
         }
         public PostCommentDto(PostComment comment)
         {
             Id = comment.Id;
+            PostId = comment.PostId;
             Author = new UserDto(comment.Author);
             Comment = comment.Comment;
             PublishedDate = comment.PublishedDate;
