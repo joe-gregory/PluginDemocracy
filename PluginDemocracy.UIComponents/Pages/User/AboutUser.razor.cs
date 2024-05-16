cca34fe41c305e123263f8f6148b97d0715dda46
joe-gregory <josephedward2nd@gmail.com>
2024-05-16 14:07:59 -0700

    About user displays limited information on any random user

---
+++ PluginDemocracy.UIComponents/Pages/User/AboutUser.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using PluginDemocracy.API.UrlRegistry;
+using PluginDemocracy.DTOs;
+
+namespace PluginDemocracy.UIComponents.Pages.User
+{
+    public partial class AboutUser
+    {
+        [Inject]
+        private Services Services { get; set; } = default!;
+        [Inject]
+        private BaseAppState AppState { get; set; } = default!;
+        [SupplyParameterFromQuery(Name = "userId")]
+        public int? UserId { get; set; }
+        UserDto? User { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            base.OnInitialized();
+            //Make get request for user base of user id
+            string url = ApiEndPoints.AboutUser + "?userId=" + UserId;
+            if (UserId.HasValue) User = await Services.GetDataGenericAsync<UserDto>(url);
+
+        }
+    }
+}
