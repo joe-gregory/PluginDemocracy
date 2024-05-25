e3127022104381b70fbda3fe64985dc731df25c7
joe-gregory <josephedward2nd@gmail.com>
2024-05-24 17:06:25 -0700

    Created new project to encapsulate Translations: PluginDemocracy.Translations. This is now used by UIComponents project and all new code in Models is using it as well.

---
+++ PluginDemocracy.Translations/ResourceKeys.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemocracy.Translations
+{
+    public static class ResourceKeys
+    {
+        public const string NewPasswordTitle = "NewPasswordTitle";
+        public const string PasswordsDoNotMatch = "PasswordsDoNotMatch";
+        public const string Password = "Password";
+        public const string ForgotPassword = "ForgotPassword?";
+        public const string ChangePassword = "ChangePassword";
+        public const string UpdateAccountInformation = "UpdateAccountInformation";
+        public const string Update = "Update";
+        public const string PleaseConfirmEmailForFullFunctionality = "PleaseConfirmEmailForFullFunctionality";
+        public const string UpdateProfilePicture = "UpdateProfilePicture";
+        public const string PleaseSelectAFile = "PleaseSelectAFile";
+        public const string Select = "Select";
+        public const string YouHaveNoCommunities = "YouHaveNoCommunities";
+        public const string RegisterCommunity = "RegisterCommunity";
+        public const string OfficialCurrency = "OfficialCurrency";
+        public const string Notifications = "Notifications";
+
+        public const string Name = "Name";
+        public const string Description = "Description";
+        public const string SelectAllOfficialLanguages = "SelectAllOfficialLanguages";
+        public const string English = "English";
+        public const string Spanish = "Spanish";
+        public const string Required = "Required";
+        public const string Register = "Register";
+        public const string Homes = "Homes";
+        public const string AddHomesAutomaticallyDescription = "AddHomesAutomaticallyDescription";
+        public const string HomesStartingNumber = "HomesStartingNumber";
+        public const string HomesEndingNumber = "HomesEndingNumber";
+        public const string InternalAddress = "InternalAddress";
+        public const string Add = "Add";
+        public const string Number = "Number";
+        public const string Generate = "Generate";
+        public const string Address = "Address";
+
+        public const string Community = "Community";
+        public const string Communities = "Communities";
+        public const string Feed = "Feed";
+        public const string JoinCommunity = "JoinCommunity";
+        public const string SelectCommunityFromDropDown = "SelectCommunityFromDropDown";
+        public const string SelectHomeDescriptionInJoinCommunity = "SelectHomeDescriptionInJoinCommunity";
+        public const string JoinAHome = "JoinAHome";
+        public const string Residents = "Residents";
+        public const string HomeOwners = "HomeOwners";
+        public const string Join = "Join";
+        public const string AreYouJoiningAsAHomeOwnerOrResidentDialogContent = "AreYouJoiningAsAHomeOwnerOrResidentDialogContent";
+        public const string Resident = "Resident";
+        public const string HomeOwner = "HomeOwner";
+        public const string SendRequest = "SendRequest";
+        public const string AvailableOwnership = "AvailableOwnership";
+        public const string Percentage = "Percentage";
+        public const string ErrorMessageJoinHomeWrongPercentage = "ErrorMessageJoinHomeWrongPercentage";
+        public const string Date = "Date";
+        public const string Title = "Title";
+        public const string Read = "Read";
+        public const string DeletePost = "DeletePost";
+        public const string DeletePostCannotBeUndone = "DeletePostCannotBeUndone";
+        
+        public const string CreateProposal = "CreateProposal";
+
+        //Translations from PluginDemocracy.Models.Dictamens keys such as labelKep and DescriptionKey of UIInputAttribute
+        public const string CreateArticleDictamenTitle = "CreateArticleDictamenTitle";
+        public const string CreateArticleDictamenDescription = "CreateArticleDictamenDescription";
+    }
+}
---
+++ PluginDemocracy.Translations/TranslationResourceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using PluginDemocracy.Translations;
+
+namespace PluginDemocracy.Translations
+{
+    public class TranslationResourceManager : INotifyPropertyChanged
+    {
+        public CultureInfo Culture { get => TranslationResources.Culture; }
+        private TranslationResourceManager()
+        {
+            if(TranslationResources.Culture == null) TranslationResources.Culture = CultureInfo.GetCultureInfo("en-US");
+            TranslationResources.Culture = CultureInfo.CurrentCulture;
+        }
+        public static TranslationResourceManager Instance { get; } = new();
+        public string this[string resourceKey] => TranslationResources.ResourceManager.GetObject(resourceKey, TranslationResources.Culture) as string ?? "No matching translation found";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void SetCulture(CultureInfo culture)
+        {
+            TranslationResources.Culture = culture;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+    }
+}
