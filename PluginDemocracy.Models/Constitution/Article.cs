a712de3c4c34bad3ad0b9ddd9cf4077acb92121d
joe-gregory <josephedward2nd@gmail.com>
2024-05-24 17:05:00 -0700

    Removed GUID. Added attributes for easy frontend processing.

--- PluginDemocracy.Models/Constitution/Article.cs
+++ PluginDemocracy.Models/Constitution/Article.cs
@@ -1,10 +1,15 @@
-﻿namespace PluginDemocracy.Models
+﻿using PluginDemocracy.Models.Attributes;
+using PluginDemocracy.Translations;
+namespace PluginDemocracy.Models
 {
     public class Article
     {
         public int Id { get; set; }
+        [UIInput]
         public string? Title { get; set; }
+        [UIInput(type : UIInputType.TextArea)]
         public string? Body { get; set; }
+        [UIInput(type : UIInputType.Integer /*, descriptionKey: ResourceKeys.*/)]
         public int? Number { get; set; }
         public DateTime? CreationDate { get; }
         public DateTime? ExpirationDate { get; set; }
