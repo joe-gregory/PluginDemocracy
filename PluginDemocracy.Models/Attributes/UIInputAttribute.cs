24764575dc923a30f06612bc2c575e02ee23406d
joe-gregory <josephedward2nd@gmail.com>
2024-05-31 16:59:21 -0700

    Attributes to programatically handle forms on frontend

--- PluginDemocracy.Models/Attributes/UIInputAttribute.cs
+++ PluginDemocracy.Models/Attributes/UIInputAttribute.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace PluginDemocracy.Models.Attributes
 {
+    /// <summary>
+    /// This attribute tells the frontend how to render the input element for the property.This way the frontend can be built
+    /// programatically given that Dictamens can be of many different types. 
+    /// </summary>
+    /// <param name="type"><see cref="UIInputType"/></param>
+    /// <param name="labelKey">This is the key in the ResourceKeys to know which translation to apply to use as the label of the element</param>
+    /// <param name="descriptionKey">This is the key in the ResourceKeys to know which translation to apply for the description</param>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-    sealed class UIInputAttribute(UIInputType type = UIInputType.Null, string? labelKey = null, string? descriptionKey = null) : Attribute
+    sealed class UIInputAttribute(UIInputType type = UIInputType.Null, string? labelKey = null, string? descriptionKey = null, bool optional = true) : Attribute
     {
         /// <summary>
         /// The type of input to be used in the frontend. Options are of type <see cref="UIInputType"/>."/>
@@ -17,6 +24,10 @@ namespace PluginDemocracy.Models.Attributes
         /// This is the key to use in the frontend translation resource files to obtain the label. 
         /// </summary>
         public string? LabelKey { get; } = labelKey;
+        /// <summary>
+        /// Is this parameter optional?
+        /// </summary>
+        public bool Optional { get; } = optional;
         /// <summary>
         /// This is the key to use in the translation resource files to obtain the description on front end.
         /// </summary>
---
+++ PluginDemocracy.Models/Attributes/UIInputContainerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDemocracy.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class UIInputContainerAttribute(string titleKey, string descriptionKey) : Attribute
+    {
+        public string TitleKey { get; } = titleKey;
+        public string DescriptionKey { get; } = descriptionKey;
+    }
+}
