f86d2f305e4378b02bf3974dbb21d48614ffe364
joe-gregory <josephedward2nd@gmail.com>
2024-05-31 17:00:29 -0700

    Dictamen for creating Articles for Constitution

--- PluginDemocracy.Models/Dictamens/Constitution/CreateArticleDictamen.cs
+++ PluginDemocracy.Models/Dictamens/Constitution/CreateArticleDictamen.cs
@@ -9,16 +9,15 @@ namespace PluginDemocracy.Models.Dictamens.Constitution
 {
     public class CreateArticleDictamen : BaseDictamen
     {
-        public override string? Title { get => ResourceKeys.CreateArticleDictamenTitle ; set { } } 
-        public override string? Description { get => ResourceKeys.CreateArticleDictamenDescription; set { } } 
+        public override string? TitleKey { get => ResourceKeys.CreateArticleDictamenTitle ; set { } } 
+        public override string? DescriptionKey { get => ResourceKeys.CreateArticleDictamenDescription; set { } }
         #region InputsNeededByThisDictamenInParticular
-        //I need to know the details of the law to be known. What all is part of a law? 
-        //I need to know if it is going to have a specific number? (if so, push all others)
-        //How am I going to handle the inputs? Get them manually in here or import them from Article.cs? What is the best way to do it? 
+        public Article Article { get; set; } = new();
         #endregion
         public override void Execute()
         {
-            throw new NotImplementedException();
+            if (Community == null) throw new Exception("This Dictamen's Community is not set");
+            Community.Constitution.AddArticle(Article);
         }
     }
 }
