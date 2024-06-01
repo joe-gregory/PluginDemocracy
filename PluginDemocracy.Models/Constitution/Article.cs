fad3c47a12d36d7719dc58e6b4534a847bac2441
joe-gregory <josephedward2nd@gmail.com>
2024-05-31 17:00:13 -0700

    Changes to Article type

--- PluginDemocracy.Models/Constitution/Article.cs
+++ PluginDemocracy.Models/Constitution/Article.cs
@@ -2,19 +2,21 @@
 using PluginDemocracy.Translations;
 namespace PluginDemocracy.Models
 {
+    [UIInputContainer(titleKey: ResourceKeys.ArticleUIContainerTitle, descriptionKey: ResourceKeys.ArticleUIContainerDescription)]
     public class Article
     {
         public int Id { get; set; }
-        [UIInput]
+        [UIInput(type:UIInputType.TextBox, labelKey: ResourceKeys.ArticleTitleUIInputLabel,descriptionKey: ResourceKeys.ArticleTitleUIInputDescription)]
         public string? Title { get; set; }
-        [UIInput(type : UIInputType.TextArea)]
+        [UIInput(type : UIInputType.TextArea, labelKey: ResourceKeys.ArticleBodyUIInputLabel, descriptionKey: ResourceKeys.ArticleBodyUIInputDescription, optional: false)]
         public string? Body { get; set; }
-        [UIInput(type : UIInputType.Integer /*, descriptionKey: ResourceKeys.*/)]
+        [UIInput(type : UIInputType.Integer, labelKey: ResourceKeys.ArticleNumberLabel, descriptionKey: ResourceKeys.ArticleNumberDescription)]
         public int? Number { get; set; }
         public DateTime? CreationDate { get; }
+        [UIInput(type : UIInputType.Date, labelKey: ResourceKeys.ArticleExpirationDateUIInputLabel, descriptionKey: ResourceKeys.ArticleExpirationDateUIInputDescription)]
         public DateTime? ExpirationDate { get; set; }
         public readonly BaseDictamen? OriginDictamen;
-        protected Article() {}
+        public Article() {}
         public Article(BaseDictamen dictamen, string title, string body, int? number, DateTime? expirationDate)
         {
             OriginDictamen = dictamen;
