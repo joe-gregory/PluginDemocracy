db1cc44626493d5f10cda488505fed81c3a820c7
joe-gregory <josephedward2nd@gmail.com>
2024-06-01 18:33:48 -0700

    work on create petition page

--- PluginDemocracy.UIComponents/Pages/Community/CreatePetition.razor.cs
+++ PluginDemocracy.UIComponents/Pages/Community/CreatePetition.razor.cs
@@ -1,9 +1,17 @@
-﻿using PluginDemocracy.DTOs;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using PluginDemocracy.DTOs;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace PluginDemocracy.UIComponents.Pages.Community
 {
     public partial class CreatePetition
     {
-        private readonly PetitionDTO _petition = new();
+        private readonly PetitionDTO Petition = new();
+        private readonly IList<IBrowserFile> files = [];
+        protected override void OnInitialized()
+        {
+            if (AppState?.User != null) Petition.Authors.Add(AppState.User);
+            if (AppState?.User?.Citizenships.Count == 1) Petition.CommunityDTO = AppState.User.Citizenships[0];
+        }
     }
 }
