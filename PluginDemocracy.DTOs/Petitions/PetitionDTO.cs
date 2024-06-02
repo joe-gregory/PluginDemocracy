76b22b89d24b9325cdeedf332603455b55ac3984
joe-gregory <josephedward2nd@gmail.com>
2024-06-01 18:32:34 -0700

    Added property

--- PluginDemocracy.DTOs/Petitions/PetitionDTO.cs
+++ PluginDemocracy.DTOs/Petitions/PetitionDTO.cs
@@ -3,13 +3,17 @@
     public class PetitionDTO
     {
         public int Id { get; set; }
-        public CommunityDTO? CommunityDTO { get; set; }
         public bool Published { get; set; }
         public DateTime? PublishedDate { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public CommunityDTO? CommunityDTO { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public List<UserDTO> Authors { get; set; } = [];
         public string? ActionRequested { get; set; }
+        public string? SupportingArguments { get; set; }
+        public DateTime? DeadlineForResponse { get; set; }
         public List<string> LinksToSupportingDocuments { get; set; } = [];
+        public List<UserDTO> Authors { get; set; } = [];
+
     }
 }
