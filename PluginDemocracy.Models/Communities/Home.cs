2faedb0b6b9f94c090c3e9f0769fd5dc4db92992
joe-gregory <josephedward2nd@gmail.com>
2024-07-02 09:08:29 -0700

    Refactoring of Home model

--- PluginDemocracy.Models/Communities/Home.cs
+++ PluginDemocracy.Models/Communities/Home.cs
@@ -2,132 +2,140 @@
 
 namespace PluginDemocracy.Models
 {
-    public class Home : BaseCitizen
+    public class Home
     {
-        public Community? ParentCommunity { get; set; }
-        public ICollection<HomeOwnership> Ownerships { get; set; }
-        [NotMapped]
-        public override string FullName
-        {
-            get
-            {
-                string fullName = string.Empty;
-                foreach (HomeOwnership hw in Ownerships) fullName += hw.Owner.FullName + ", ";
-                return fullName;
-            }
-        }
-        [NotMapped]
-        public override string Initials
-        {
-            get
-            {
-                return $"{Number}{ParentCommunity?.Initials}";
-            }
-        }
-        [NotMapped]
-        public override List<Community> Citizenships
-        {
-            get
-            {
-                if (ParentCommunity != null) return [ParentCommunity];
-                else return [];
-            }
-        }
+        public int Id { get; init; }
         public int Number { get; set; }
         public string InternalAddress { get; set; }
-        [NotMapped]
-        public override string Address { get => Number + " " + InternalAddress + "/n" + ParentCommunity?.Address; }
-        [NotMapped]
-        public Dictionary<BaseCitizen, double> OwnersWithOwnership
+        public HOACommunity? Community { get; set; }
+        /// <summary>
+        /// Get only property. Returns Number + InternalAddress + Community.Address.
+        /// </summary>
+        public string FullAddress 
+        { 
+            get 
+            { 
+                return $"{Number} {InternalAddress}.\n{Community?.Address}"; 
+            } 
+        }
+        private readonly List<HomeOwnership> _ownerships;
+        /// <summary>
+        /// Get only property. Returns all the ownerships of this home.
+        /// </summary>
+        public IReadOnlyList<HomeOwnership> Ownerships
+        {
+            get
+            {
+                return _ownerships.AsReadOnly();
+            }
+        }
+        /// <summary>
+        /// Get only property that returns a dictionary of owners and their ownership percentage of this home. 
+        /// </summary>
+        public Dictionary<User, double> OwnersOwnerships
         {
             get => Ownerships.Where(o => o.Owner != null).ToDictionary(o => o.Owner!, o => o.OwnershipPercentage);
         }
-        [NotMapped]
-        public List<BaseCitizen> Owners => [.. OwnersWithOwnership.Keys];
-        [NotMapped]
+        /// <summary>
+        /// Get only property that returns a list of owners of this home.
+        /// </summary>
+        public List<User> Owners 
+        {
+            get 
+            {
+                return [.. OwnersOwnerships.Keys];
+            }
+        }
+        /// <summary>
+        /// Get only property that returns the currently claimed ownership percentage of this home up to 100%.
+        /// </summary>
         public double CurrentlyOwnedPercentage
         {
             get
             {
-                return OwnersWithOwnership.Values.Sum();
+                return OwnersOwnerships.Values.Sum();
             }
         }
-        [NotMapped]
+        /// <summary>
+        /// Get only property. It returns the available ownership percentage that can be claimed by new owners.
+        /// </summary>
         public double AvailableOwnershipPercentage
         {
             get
             {
-                return 100 - OwnersWithOwnership.Values.Sum();
+                return 100 - OwnersOwnerships.Values.Sum();
             }
         }
-        public List<User> Residents { get; set; }
+        private readonly List<User> _residents;
         /// <summary>
-        /// You are a Citizen of this home if you are either an owner or a resident of Home. home.AddOwner, AddResident, etc need to happen in the GatedCommunity so that
-        /// Citizen.Citizenships can be updated for both the GatedCommunity and the Home. The Home doesn't have access to its parent GatedCommunity, so it must be done in the
-        /// parent GatedCommunity in order to maintain Citizen.Citizenships.
+        /// Residents are individuals that live in this home but that do not own it. 
         /// </summary>
-        [NotMapped]
-        public List<BaseCitizen> Citizens
-        {
-            get => OwnersWithOwnership.Keys.Union(Residents).ToList();
+        public IReadOnlyList<User> Residents 
+        { 
+            get 
+            { 
+                return _residents.AsReadOnly();
+            } 
         }
-        public Home() : base()
+        /// <summary>
+        /// Get only property. You are a Citizen of this home if you are either an owner or a resident of Home. 
+        /// </summary>
+        public List<User> Citizens
         {
-            InternalAddress = string.Empty;
-            Ownerships = new HashSet<HomeOwnership>();
-            Residents = [];
+            get => OwnersOwnerships.Keys.Union(Residents).ToList();
+        }
+        //Disabling CS8618 as this is a parameterless constructor for the benefit of EF Core
+        #pragma warning disable CS8618
+        private Home(){}
+        #pragma warning restore CS8618
+        public Home(HOACommunity community, int number, string internalAddress)
+        {
+            Community = community;
+            Number = number;
+            InternalAddress = internalAddress;
+            _ownerships = [];
+            _residents = [];
         }
         /// <summary>
         /// Adds owner with percentage or updates owners percentage
         /// </summary>
-        /// <param name="citizen"></param>
+        /// <param name="user"></param>
         /// <param name="percentage"></param>
         /// <exception cref="ArgumentException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public void AddOwner(BaseCitizen citizen, double percentage)
+        public void AddOwner(User user, double percentage)
         {
-            if (citizen == null) throw new ArgumentException("citizen cannot be null");
+            if (user == null) throw new ArgumentException("citizen cannot be null");
             if (percentage <= 0 || percentage > 100) throw new ArgumentException("Ownership percentage needs to be between 1 and 100");
             if (percentage > AvailableOwnershipPercentage) throw new ArgumentException("Total ownership percentage exceeds 100. Readjust for this or other owners.");
-            HomeOwnership newHomeOwnership = new(this, citizen, percentage);
-            Ownerships.Add(newHomeOwnership);
-            citizen.HomeOwnerships.Add(newHomeOwnership);
+            HomeOwnership newHomeOwnership = new(this, user, percentage);
+            _ownerships.Add(newHomeOwnership);
+            user.AddHomeOwnership(newHomeOwnership);
         }
-        /// <summary>
-        /// </summary>
-        /// <param name="Citizen"></param>
-        public void RemoveOwner(BaseCitizen citizen)
+        public void RemoveOwner(User citizen)
         {
             if (citizen == null) throw new ArgumentException("Citizen cannot be null");
 
             var ownership = Ownerships.FirstOrDefault(o => o.Owner == citizen);
             if (ownership != null)
             {
-                Ownerships.Remove(ownership);
-                citizen.HomeOwnerships.Remove(ownership);
+                _ownerships.Remove(ownership);
+                citizen.RemoveHomeOwnership(ownership);
             }
         }
-        /// <summary>
-        /// This method should be called on parent Gated Community to correctly update Citizen.Citizenships
-        /// </summary>
-        /// <param name="citizen"></param>
         public void AddResident(User citizen)
         {
             if (citizen == null) throw new ArgumentException("Citizen cannot be null");
 
-            Residents.Add(citizen);
-            citizen.ResidentOfHomes.Add(this);
+            _residents.Add(citizen);
+            citizen.AddAsResidentOfHome(this);
         }
-        /// <summary>
-        /// This method should be called on parent Gated Community to correctly update Citizen.Citizenships
-        /// </summary>
-        /// <param name="citizen"></param>
         public void RemoveResident(User citizen)
         {
             if (citizen == null) throw new ArgumentException("Citizen cannot be null");
 
-            if (Residents.Contains(citizen)) Residents.Remove(citizen);
-            if (citizen.ResidentOfHomes.Contains(this)) citizen.ResidentOfHomes.Remove(this);
+            if (Residents.Contains(citizen)) _residents.Remove(citizen);
+            if (citizen.ResidentOfHomes.Contains(this)) citizen.RemoveAsResidentOfHome(this);
         }
     }
 }
