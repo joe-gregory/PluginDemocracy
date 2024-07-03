5ad4d8689d55e024672d809faad10ec23d40fd01
joe-gregory <josephedward2nd@gmail.com>
2024-07-03 16:46:32 -0700

    DbSets in Data proj

--- PluginDemocracy.Data/PluginDemocracyContext.cs
+++ PluginDemocracy.Data/PluginDemocracyContext.cs
@@ -3,8 +3,13 @@ using PluginDemocracy.Models;
 
 namespace PluginDemocracy.Data
 {
-    public class PluginDemocracyContext : DbContext
+    public class PluginDemocracyContext(DbContextOptions<PluginDemocracyContext> options) : DbContext(options)
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ResidentialCommunity> Communities { get; set; }
+        public DbSet<JoinCommunityRequest> JoinCommunityRequests { get; set; }
+        public DbSet<Petition> Petitions { get; set; }
+        public DbSet<Post> Posts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
         }
