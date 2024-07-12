20134553556249e0c320e672e26be69fc415065e
joe-gregory <josephedward2nd@gmail.com>
2024-07-12 14:17:19 -0700

    New migration

--- PluginDemocracy.Data/Migrations/20240712175738_EncapsulatedCollectionsOfJoinCommunityRequests.Designer.cs
+++ PluginDemocracy.Data/Migrations/20240712175738_EncapsulatedCollectionsOfJoinCommunityRequests.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PluginDemocracy.Data;
 
@@ -11,9 +12,11 @@ using PluginDemocracy.Data;
 namespace PluginDemocracy.Data.Migrations
 {
     [DbContext(typeof(PluginDemocracyContext))]
-    partial class PluginDemocracyContextModelSnapshot : ModelSnapshot
+    [Migration("20240712175738_EncapsulatedCollectionsOfJoinCommunityRequests")]
+    partial class EncapsulatedCollectionsOfJoinCommunityRequests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PluginDemocracy.Data/Migrations/20240712175738_EncapsulatedCollectionsOfJoinCommunityRequests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PluginDemocracy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EncapsulatedCollectionsOfJoinCommunityRequests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinksToFiles",
+                table: "JoinCommunityRequests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LinksToFiles",
+                table: "JoinCommunityRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
