f94b30af77dbd18c575dbbf566adac8b3b05905f
joe-gregory <josephedward2nd@gmail.com>
2024-07-20 07:15:54 -0700

    Changes to About Community (back to separate files) and currently trying to fix issue of read only collections loading from EFC when loging in

---
+++ PluginDemocracy.UIComponents/Pages/Community/AboutCommunity.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using PluginDemocracy.API.UrlRegistry;
+using PluginDemocracy.DTOs;
+
+namespace PluginDemocracy.UIComponents.Pages.Community
+{
+    public partial class AboutCommunity
+    {
+        [SupplyParameterFromQuery]
+        protected int? CommunityId { get; set; }
+        protected ResidentialCommunityDTO? CommunityDTO;
+        //protected override async Task OnInitializedAsync()
+        //{
+        //    CommunityDTO = await Services.GetDataGenericAsync<ResidentialCommunityDTO>($"{ApiEndPoints.GetCommunityAbout}?communityId={CommunityId}");
+        //}
+    }
+}
